ddfc38f678c213a906073a6fe9c080c2ec66dbd0
Najwaageel <Najwa.Ageel.abt.js@tuwaiq.edu.sa>
2021-10-28 13:09:42 +0300

    najwa

---
+++ About5.js
@@ -0,0 +1,10 @@
+const button = document.querySelector(".btn")
+const alerts = document.querySelector(".alert")
+
+button.addEventListener("click", function () {
+
+        alerts.innerHTML = `<h4> Thank you , My greetings Najwa AL-Thobiti .</h4>`
+
+    })
+
+
---
+++ todolist.js
@@ -0,0 +1,8 @@
+const button = document.querySelector(".btn")
+const text = document.querySelector(".text")
+
+button.addEventListener("click", function ( ) {
+    const messages = "Thank you, we will try to add it again ."
+    text.innerHTML = messages
+
+})
